e454da0df8d320bac3599a86835e811c46898564
jop922 <joao.paulo.cruz@outlook.com>
2024-09-02 16:43:45 -0300

    teste de conexão ODBC

--- services/database.py
+++ services/database.py
@@ -1,10 +1,23 @@
 import pyodbc
 
 
-server = 'JOAO'
-database = 'cadastro_cliente'
-username = 'Joao'
-password = '97519242'
-cnxn=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=JOAO;DATABASE=cadastro_cliente;Trusted_connection=yes')
-cursor=cnxn.cursor()
+# server = 'JOAO'
+# database = 'cadastro_cliente'
+# username = 'Joao'
+# password = '97519242'
+# cnxn=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=JOAO;DATABASE=cadastro_cliente;Trusted_connection=yes')
+# cursor=cnxn.cursor()
 
+# import pyodbc
+
+try:
+    cnxn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=JOAO;'
+        'DATABASE=cadastro_cliente;'
+        'Trusted_connection=yes;'
+    )
+    print("Conexão bem-sucedida!")
+    cnxn.close()
+except pyodbc.Error as e:
+    print(f"Erro de conexão: {e}")
